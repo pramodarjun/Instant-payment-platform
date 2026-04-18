28265b86cce058334529895582533c31c31eed0d
pramodarjun <pramodarjun@gmail.com>
2026-04-18 13:46:48 +0530

    initial commit to push two services

---
+++ payment-ingestor/src/main/java/com/payments/ingestor/PaymentIngestorApplication.java
@@ -0,0 +1,11 @@
+package com.payments.ingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIngestorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentIngestorApplication.class, args);
+    }
+}
